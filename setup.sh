db9b5711797143b43072884b79c53b12eb0b1104
yao.yu <yao.yu@wenba100.com>
2016-10-10 18:08:05 +0800

    modify setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-echo "开始配置和安装vim......"
+echo "开始安装和配置vim......"
 
-mv -f ~/.vimrc ~/.vimrc.back
+if [ -f "~/.vimrc" ]; then
+	mv -f ~/.vimrc ~/.vimrc.back
+fi
 cp -f .vimrc ~/.vimrc
+
+vimDir="/home/wenba/.vim/"
+if [ ! -d "$vimDir" ]; then
+	mkdir "$vimDir"
+fi
